6a8adf0e872879ca877bdb66a96aa9283acb4f49
Steven Chicas <huezosteven402@gmail.com>
2026-02-05 17:42:41 -0600

    Tarea realizada: agregarle a la calculadora las opciones de  "factorial, porcentaje, potencia,raiz "

--- CALCULADORA/app/src/main/java/com/example/calculadora/MainActivity.java
+++ CALCULADORA/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -32,34 +32,62 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void calcular() {
-        tempVal = findViewById(R.id.txtNum1);
-        Double num1 = Double.parseDouble(tempVal.getText().toString());
 
-        tempVal  = findViewById(R.id.txtNum2);
-        Double num2 = Double.parseDouble(tempVal.getText().toString());
+            tempVal = findViewById(R.id.txtNum1);
+            Double num1 = Double.parseDouble(tempVal.getText().toString());
 
-        double respuesta = 0;
+            tempVal  = findViewById(R.id.txtNum2);
+            Double num2 = Double.parseDouble(tempVal.getText().toString());
 
-        opt = findViewById(R.id.optSuma);
-        if (opt.isChecked()) {
-            respuesta = num1 + num2;
+            double respuesta = 0;
 
+            opt = findViewById(R.id.optSuma);
+            if (opt.isChecked()) {
+                respuesta = num1 + num2;
+            }
+
+            opt = findViewById(R.id.optResta);
+            if (opt.isChecked()) {
+                respuesta = num1 - num2;
+            }
+
+            opt = findViewById(R.id.optMultiplicar);
+            if (opt.isChecked()) {
+                respuesta = num1 * num2;
+            }
+
+            opt = findViewById(R.id.optDividir);
+            if (opt.isChecked()) {
+                respuesta = num1 / num2;
+            }
+
+            opt = findViewById(R.id.optFactorial);
+            if (opt.isChecked()) {
+                double factorial = 1;
+                int n = (int) Math.round(num1);
+                for (int i = 1; i <= n; i++) {
+                    factorial = factorial * i;
+                }
+                respuesta = factorial;
+            }
+
+            opt = findViewById(R.id.optPorcentaje);
+            if (opt.isChecked()) {
+                respuesta = (num1 * num2) / 100;
+            }
+
+            opt = findViewById(R.id.optPotencia);
+            if (opt.isChecked()) {
+                respuesta = Math.pow(num1, num2);
+            }
+
+            opt = findViewById(R.id.optRaiz);
+            if (opt.isChecked()) {
+                respuesta = Math.sqrt(num1);
+            }
+
+            tempVal = findViewById(R.id.lblRespuesta);
+            tempVal.setText("Respuesta: "+ respuesta);
         }
-
-        opt = findViewById(R.id.optResta);
-        if (opt.isChecked()) {
-            respuesta = num1 - num2;
-
-        }
-
-        opt = findViewById(R.id.optMultiplicar);
-        if (opt.isChecked()) {
-            respuesta = num1 * num2;
-
-        }
-
-        tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
-
     }
-}
+
