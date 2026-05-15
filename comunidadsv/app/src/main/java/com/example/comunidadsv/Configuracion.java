73326473786062a494e380c593e0ae685f72a96d
Steven Chicas <huezosteven402@gmail.com>
2026-05-14 18:19:37 -0600

    Funciona registrarse desde el cellll

--- comunidadsv/app/src/main/java/com/example/comunidadsv/Configuracion.java
+++ comunidadsv/app/src/main/java/com/example/comunidadsv/Configuracion.java
@@ -1,8 +1,7 @@
 package com.example.comunidadsv;
 
 public class Configuracion {
-    // Para el emulador de Android, localhost de la PC es 10.0.2.2
-    public static final String SERVIDOR = "http://10.0.2.2:5984";
+    public static final String SERVIDOR = "http://josue:IdJosue2004@192.168.1.8:5984";
     public static final String USER = "josue";
     public static final String PASS = "IdJosue2004";
 }
