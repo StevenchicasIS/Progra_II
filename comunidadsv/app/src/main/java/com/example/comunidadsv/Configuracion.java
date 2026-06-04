e143b24b790ecb51ae6c6bade72e5fb69026f6ae
Josuegarcia2 <jsuegca21@gmail.com>
2026-06-04 08:20:27 -0600

    REd listo

--- comunidadsv/app/src/main/java/com/example/comunidadsv/Configuracion.java
+++ comunidadsv/app/src/main/java/com/example/comunidadsv/Configuracion.java
@@ -1,7 +1,16 @@
 package com.example.comunidadsv;
 
 public class Configuracion {
-    public static final String SERVIDOR = "http://josue:IdJosue2004@192.168.1.8:5984";
+
+
+    private static final String SERVER_IP = "192.168.1.8";  // <-- CAMBIA SOLO ESTA LÍNEA
+    private static final String SERVER_PORT = "5984";
     public static final String USER = "josue";
     public static final String PASS = "IdJosue2004";
+
+    public static final String SERVIDOR = "http://" + USER + ":" + PASS + "@" + SERVER_IP + ":" + SERVER_PORT;
+
+    public static String getCurrentServerInfo() {
+        return "Conectado a: " + SERVER_IP + ":" + SERVER_PORT;
+    }
 }
